49d3ad3aa2819cc9608161dc7ae1907b7542f715
KatherineMichel <kthrnmichel@gmail.com>
2015-05-05 16:34:39 -0500

    Update and rename chris-dixon-thesis to chris-dixon-thesis.m

---
+++ chris-dixon-thesis.m
@@ -0,0 +1,13 @@
+Blog
+*   [Chris Dixon Blog](http://cdixon.org)
+
+Ideas
+*   [The next big thing is sitting right in front of you](http://cdixon.org/2011/07/03/the-next-big-thing-is-sitting-right-in-front-of-you)
+*   [Developing new startup ideas](http://cdixon.org/2010/03/14/developing-new-startup-ideas)
+*   [The next big thing will start out looking like a toy](http://cdixon.org/2010/01/03/the-next-big-thing-will-start-out-looking-like-a-toy)
+*   [“A lot of the best tech startups are ideas that have been around for years”](http://cdixon.org/2014/10/15/a-lot-of-the-best-tech-startups-are-ideas-that-have-been-around-for-years)
+*   [What the smartest people do on the weekend is what everyone else will do during the week in ten years](http://cdixon.org/2013/03/03/what-the-smartest-people-do-on-the-weekend-is-what-everyone-else-will-do-during-the-week-in-ten-years)
+
+
+
+
